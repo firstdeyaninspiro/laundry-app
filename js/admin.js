95f152188e3e3b0f5b8155c42a610d232d80eb00
firstdeyaninspiro <firstdeyan.putra@inspirotechs.com>
2024-05-25 11:08:44 +0700

    update

--- js/admin.js
+++ js/admin.js
@@ -1,3 +1,31 @@
+function setEyeCatchingTitle() {
+    const title = document.querySelector('h1');
+    title.style.color = "#fff"; // Mengatur warna teks menjadi putih
+    title.style.textShadow = "2px 2px 4px rgba(0, 0, 0, 0.5)"; // Menambahkan bayangan teks
+    title.style.padding = "10px 20px"; // Menambahkan padding untuk judul teks
+    title.style.backgroundColor = "rgba(0, 0, 0, 0.5)"; // Menambahkan kotak belakang dengan warna hitam transparan
+    title.style.borderRadius = "8px"; // Mengatur sudut kotak belakang
+
+    const titles = document.querySelectorAll('h2, h3, h4'); // Pilih semua elemen h2, h3, dan h4
+
+    titles.forEach(title => {
+        title.style.color = "#fff"; // Mengatur warna teks menjadi putih
+        title.style.textShadow = "2px 2px 4px rgba(0, 0, 0, 0.5)"; // Menambahkan bayangan teks
+        title.style.padding = "10px 20px"; // Menambahkan padding untuk judul teks
+        title.style.backgroundColor = "rgba(0, 0, 0, 0.5)"; // Menambahkan kotak belakang dengan warna hitam transparan
+        title.style.borderRadius = "8px"; // Mengatur sudut kotak belakang
+        title.style.display = "inline-block"; // Membuat judul teks menjadi inline block agar kotak belakangnya mengikuti panjang teks
+    });
+}
+
+function setStylishBackground() {
+    document.body.style.backgroundImage = "url('img/bg.jpg')";
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundAttachment = "fixed";
+    document.body.style.backgroundPosition = "center";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundBlendMode = "darken";
+}
 function loadBookings(filteredBookings) {
     const bookingsContainer = document.getElementById('bookings');
     let bookings = localStorage.getItem('bookings');
@@ -7,24 +35,77 @@ function loadBookings(filteredBookings) {
 
     const bookingsToDisplay = filteredBookings || bookings;
 
+    // Membuat card atau blok untuk form filter
+    const filterCard = document.createElement('div');
+    filterCard.className = 'card mb-3';
+    const filterCardBody = document.createElement('div');
+    filterCardBody.className = 'card-body';
+
+    // Membuat form filter
+    const filterForm = document.createElement('form');
+    filterForm.className = 'form-inline';
+
+    // Menambahkan elemen form filter
+    filterForm.innerHTML = `
+            <div class="form-group mr-2" >
+            <input type="text" class="form-control" id="filterName" placeholder="Nama">
+        </div>
+        <div class="form-group mr-2" >
+            <input type="text" class="form-control" id="filterKodeBooking" placeholder="Kode Pesanan">
+        </div>
+        <div class="form-group mr-2" >
+            <select class="form-control" id="filterStatus" style="margin-right: 10px;">
+                <option value="">Semua Status</option>
+                <option value="Belum Disetujui">Belum Disetujui</option>
+                <option value="Disetujui">Disetujui</option>
+                <option value="Sedang Diproses">Sedang Diproses</option>
+                <option value="Selesai">Selesai</option>
+                <option value="Dibatalkan">Dibatalkan</option>
+            </select>
+        </div>
+
+        <button type="button" class="btn btn-primary" onclick="filterBookings()">Filter</button>
+        <button type="button" class="btn btn-secondary ml-2" onclick="resetFilter()">Reset</button>
+    `;
+
+    // Menambahkan form filter ke dalam card atau blok
+    filterCardBody.appendChild(filterForm);
+    filterCard.appendChild(filterCardBody);
+
+    // Menambahkan card atau blok filter ke dalam container daftar pesanan
+    bookingsContainer.appendChild(filterCard);
+
+    // Memproses dan menampilkan daftar pesanan
     bookingsToDisplay.forEach((booking, index) => {
         const bookingDiv = document.createElement('div');
         bookingDiv.className = 'booking';
         bookingDiv.innerHTML = `
-            <div class="card mb-3">
+            <div class="card mb-3 ">
                 <div class="card-body">
                     <strong>Nama:</strong> ${booking.name}<br>
                     <strong>Paket:</strong> ${booking.package}<br>
                     <strong>Kode Pesanan:</strong> ${booking.kodebooking ? booking.kodebooking : 'Belum Disetujui'}<br>
                     <strong>Status:</strong> ${booking.status}<br>
-                    <strong>Jadwal:</strong> ${booking.date}<br>
-                    <button class="btn btn-info btn-sm mt-2" onclick="showBookingDetail(${index})">Detail</button>
+                    <strong>Jadwal Penyerahan Cucian:</strong> ${booking.date}<br>
+                    <button class="btn btn-info btn-sm mt-2" onclick="showBookingDetail(${booking.originalIndex})">Detail</button>
                 </div>
             </div>
         `;
         bookingsContainer.appendChild(bookingDiv);
     });
+
+    // Menyesuaikan lebar button Filter dan Reset
+    const filterButton = filterCardBody.querySelector('.btn-primary');
+    const resetButton = filterCardBody.querySelector('.btn-secondary');
+
+    // Set lebar button
+    filterButton.style.width = '80px'; // Ubah nilai '100px' sesuai kebutuhan
+    resetButton.style.width = '80px'; // Ubah nilai '100px' sesuai kebutuhan
+
+    setEyeCatchingTitle();
+    setStylishBackground();
 }
+
 
 function filterBookings() {
     const filterName = document.getElementById('filterName').value.toLowerCase();
@@ -57,7 +138,7 @@ function resetFilter() {
     loadBookings();
 }
 
-function showBookingDetail(index) {
+function showBookingDetail(originalIndex) {
     const bookingDetailPage = document.getElementById('booking-detail-page');
     const bookingListPage = document.getElementById('booking-list-page');
     const bookingDetailContainer = document.getElementById('booking-detail');
@@ -67,7 +148,7 @@ function showBookingDetail(index) {
 
     let bookings = localStorage.getItem('bookings');
     bookings = bookings ? JSON.parse(bookings) : [];
-    const booking = bookings[index];
+    const booking = bookings[originalIndex];
 
     bookingDetailContainer.innerHTML = `
         <div class="card">
@@ -76,19 +157,22 @@ function showBookingDetail(index) {
                 <strong>No. Telepon:</strong> ${booking.phone}<br>
                 <strong>Paket:</strong> ${booking.package}<br>
                 <strong>Harga:</strong> Rp${booking.price}<br>
-                <strong>Jadwal:</strong> ${booking.date}<br>
+                <strong>Jadwal Penyerahan Cucian:</strong> ${booking.date}<br>
+                <strong>Estimasi Jadwal Pengambilan:</strong> ${booking.waktuPengambilan ? booking.waktuPengambilan : '-'}<br>
+                <strong>Waktu Pemesanan:</strong> ${booking.waktuPemesanan ? booking.waktuPemesanan : '-'}<br>
                 <strong>Status:</strong> ${booking.status}<br>
                 <strong>Kode Pesanan:</strong> ${booking.kodebooking ? booking.kodebooking : 'Belum Disetujui'}<br>
                 <strong>Waktu Disetujui:</strong> ${booking.waktuDisetujui ? booking.waktuDisetujui : 'Belum Disetujui'}<br>
                 <strong>Waktu Konfirmasi:</strong> ${booking.waktuKonfimasi ? booking.waktuKonfimasi : 'Belum Dikonfirmasi'}<br>
                 <strong>Waktu Selesai:</strong> ${booking.waktuSelesai ? booking.waktuSelesai : 'Belum Selesai'}<br>
+                <strong>Waktu Dibatalkan:</strong> ${booking.waktuDibatalkan ? booking.waktuDibatalkan : '-'}<br>
                 <strong>Chat Konfirmasi:</strong><br>
-                <textarea id="chatKonfirmasi${index}" rows="6" class="form-control mb-2" readonly>${booking.chatKonfirmasi ? booking.chatKonfirmasi : ''}</textarea>
-                <button class="btn btn-info btn-sm mb-2" onclick="copyToClipboard(${index})">Copy</button><br>
-                <button class="btn btn-primary btn-sm mt-2" onclick="approveBooking(${index})">Setujui</button>
-                <button class="btn btn-warning btn-sm mt-2" onclick="confirmLaundry(${index})">Konfirmasi</button>
-                <button class="btn btn-success btn-sm mt-2" onclick="finishBooking(${index})">Selesaikan</button>
-                <button class="btn btn-danger btn-sm mt-2" onclick="cancelBooking(${index})">Batalkan</button>
+                <textarea id="chatKonfirmasi${originalIndex}" rows="6" class="form-control mb-2" readonly>${booking.chatKonfirmasi ? booking.chatKonfirmasi : ''}</textarea>
+                <button class="btn btn-info btn-sm mb-2" onclick="copyToClipboard(${originalIndex})">Copy</button><br>
+                <button class="btn btn-primary btn-sm mt-2" onclick="approveBooking(${originalIndex})">Setujui</button>
+                <button class="btn btn-warning btn-sm mt-2" onclick="confirmLaundry(${originalIndex})">Konfirmasi</button>
+                <button class="btn btn-success btn-sm mt-2" onclick="finishBooking(${originalIndex})">Selesaikan</button>
+                <button class="btn btn-danger btn-sm mt-2" onclick="cancelBooking(${originalIndex})">Batalkan</button>
             </div>
         </div>
     `;
@@ -158,7 +242,10 @@ function generateChatKonfirmasi(booking) {
         `Paket: ${booking.package}\n` +
         `Kode Pesanan: ${booking.kodebooking}\n` +
         `Harga: Rp${booking.price}\n` +
-        `Jadwal: ${booking.date}\n\n` +
+        `Jadwal Penyerahan Cucian: ${booking.date}\n` +
+        `Estimasi Jadwal Pengambilan: ${booking.waktuPengambilan}\n` +
+        `Waktu Pemesanan: ${booking.waktuPemesanan}\n\n` +
+
         `Anda dapat melakukan pembayaran non-tunai melalui transfer ke rekening berikut:\n` +
         `BCA: 21345364646 a.n Toko Laundry\n` +
         `ShopeePay: 08917483358 a.n Toko Laundry\n` +
--- js/main.js
+++ js/main.js
@@ -1,28 +1,83 @@
 document.addEventListener('DOMContentLoaded', function() {
     loadPackages();
+    setStylishBackground(); // Tambahkan fungsi untuk mengatur latar belakang
+    setEyeCatchingTitle(); 
 });
+
+function setStylishBackground() {
+    document.body.style.backgroundImage = "url('img/bg.jpg')";
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundAttachment = "fixed";
+    document.body.style.backgroundPosition = "center";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundBlendMode = "darken";
+}
+
+// Fungsi untuk mengatur teks judul agar lebih eye-catching
+function setEyeCatchingTitle() {
+    const title = document.querySelector('h1');
+    title.style.color = "#fff"; // Mengatur warna teks menjadi putih
+    title.style.textShadow = "2px 2px 4px rgba(0, 0, 0, 0.5)"; // Menambahkan bayangan teks
+    title.style.padding = "10px 20px"; // Menambahkan padding untuk judul teks
+    title.style.backgroundColor = "rgba(0, 0, 0, 0.5)"; // Menambahkan kotak belakang dengan warna hitam transparan
+    title.style.borderRadius = "8px"; // Mengatur sudut kotak belakang
+
+    const titles = document.querySelectorAll('h2, h3, h4'); // Pilih semua elemen h2, h3, dan h4
+
+    titles.forEach(title => {
+        title.style.color = "#fff"; // Mengatur warna teks menjadi putih
+        title.style.textShadow = "2px 2px 4px rgba(0, 0, 0, 0.5)"; // Menambahkan bayangan teks
+        title.style.padding = "10px 20px"; // Menambahkan padding untuk judul teks
+        title.style.backgroundColor = "rgba(0, 0, 0, 0.5)"; // Menambahkan kotak belakang dengan warna hitam transparan
+        title.style.borderRadius = "8px"; // Mengatur sudut kotak belakang
+        title.style.display = "inline-block"; // Membuat judul teks menjadi inline block agar kotak belakangnya mengikuti panjang teks
+    });
+}
 
 function loadPackages() {
     const packagesContainer = document.getElementById('packages');
     const packageSelect = document.getElementById('package');
 
     const packages = [
-        { name: 'Paket A - 1 kg 1 Hari', price: 10000 },
-        { name: 'Paket B - 1 kg 2 Hari', price: 8000 },
-        { name: 'Paket C - 1-3 kg 1 Hari', price: 18000 },
-        { name: 'Paket D - 1-3 kg 2 Hari', price: 15000 },
+        {   
+            id: 1,
+            name: 'Paket Reguler - 1 kg 3 Hari', 
+            price: 6000,
+            image: 'img/paketReguler1kg.jpg' 
+        },
+        { 
+            id: 2,
+            name: 'Paket Reguler - 2-3 kg 3 Hari', 
+            price: 15000,
+            image: 'img/paketReguler3kg.jpg' 
+        },
+        { 
+            id: 3,
+            name: 'Paket Express - 1 kg 1 Hari', 
+            price: 9000,
+            image: 'img/paketExpress1kg.jpg'
+        },
+        { 
+            id: 4,
+            name: 'Paket Express - 2-3 kg 1 Hari', 
+            price: 20000,
+            image: 'img/paketExpress3kg.jpg' 
+        },
     ];
 
     packages.forEach((pkg, index) => {
         const packageDiv = document.createElement('div');
         packageDiv.className = 'col-md-4';
         packageDiv.innerHTML = `
-            <div class="card mb-4">
+        <a href="#booking-form">
+            <div class="card mb-3">
+            <img src="${pkg.image}" class="card-img-top" alt="${pkg.name}">
                 <div class="card-body">
                     <h5 class="card-title">${pkg.name}</h5>
                     <p class="card-text">Harga: Rp ${pkg.price}</p>
                 </div>
             </div>
+        </a>
         `;
         packagesContainer.appendChild(packageDiv);
 
@@ -53,24 +108,38 @@ function submitBooking() {
     bookings = bookings ? JSON.parse(bookings) : [];
 
     const bookingDate = new Date(date);
-    const estimateCompletionDate = new Date(bookingDate);
-    estimateCompletionDate.setDate(bookingDate.getDate() + 1);
+    const currentDate = new Date().toLocaleString();
+    const originalIndex = bookings ? bookings.length : 0;
 
     const newBooking = {
+        originalIndex: originalIndex,
         name: name,
         phone: phone,
         package: selectedPackage.name,
         price: selectedPackage.price,
         date: bookingDate.toLocaleString(),
-        estimateCompletionDate: estimateCompletionDate.toLocaleString(),
+        waktuPemesanan: currentDate,
+        waktuPengambilan: '',
         status: 'Belum Disetujui',
         kodebooking: '',
         waktuDisetujui: '',
         waktuKonfimasi: '',
-        waktuSelesai: ''
+        waktuSelesai: '',
+        waktuDibatalkan: ''
     };
+
+    if (selectedPackage.id === 1 || selectedPackage.id === 2) {
+        bookingDate.setDate(bookingDate.getDate() + 3);
+    } else if (selectedPackage.id === 3 || selectedPackage.id === 4) {
+        bookingDate.setDate(bookingDate.getDate() + 1);
+    }
+
+    newBooking.waktuPengambilan = bookingDate.toLocaleString();
 
     bookings.push(newBooking);
     localStorage.setItem('bookings', JSON.stringify(bookings));
     alert('Pesanan berhasil disubmit! Kami akan segera mengirim pesan konfirmasi ke WhatsApp anda');
+
+    // Refresh the page
+    window.location.reload();
 }
